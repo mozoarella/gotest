8c69dfb08d673115a2e1b2cf70d78555a350f688
Dylan Maassen van den Brink <dylan@mvdb.eu>
2022-01-17 20:04:11 +0100

    Stupid periods

--- greetings.go
+++ greetings.go
@@ -3,6 +3,6 @@ package gotest
 import "fmt"
 
 func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v Welcome!", name)
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
 }
